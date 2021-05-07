120e961956bf38a64482eef25868297010ede584
matt bimpson <m.bimpson@gmail.com>
2021-05-07 07:48:24 -0500

    remove pointless js/css files

--- src/components/ConfirmBtn.vue
+++ src/components/ConfirmBtn.vue
@@ -9,5 +9,42 @@
         </div>
     </div>
 </template>
-<script src='./ConfirmBtn.js' />
-<style scoped src='./ConfirmBtn.css' />
+<script>
+export default {
+name: 'ConfirmBtn',
+props: ['buttonText'],
+data: function () {
+    return {
+        confirming: false
+    }
+},
+methods: {
+    toggleConfirm () {
+        this.confirming = !this.confirming
+    },
+    confirmYes () {
+        this.$emit('confirm-action')
+        this.toggleConfirm()
+    },
+    confirmNo () {
+        this.toggleConfirm()
+    }
+}
+}
+</script>
+<style scoped>
+input {
+    border-radius: 5px;
+    border: 0px;
+}
+
+.confirm-yes {
+    background-color: lightgreen;
+    padding: 5px;
+    margin-right: 5px;
+}
+.confirm-no {
+    background-color: lightcoral;
+    padding: 5px;
+}
+</style>
--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -9,5 +9,57 @@
         </div>
     </div>
 </template>
-<script src="./Todo.js" />
-<style scoped src="./Todo.css" />
+
+<script>
+import * as a from '../store/action-types'
+export default {
+    name: 'Todo',
+    props: ['todo'],
+    methods: {
+        remove () {
+            this.$store.dispatch(a.removeTodo, this.todo.id)
+        },
+        toggleCompleted () {
+            let payload = { ...this.todo }
+            payload.completed = !this.todo.completed
+            this.$store.dispatch(a.updateTodo, payload)
+        }
+    },
+    computed: {
+        completed () {
+            return this.todo.completed
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+.todo-container.completed {
+    text-decoration: line-through;
+    color: darkgray;
+    background-color: darkgreen;
+}
+
+.todo-item {
+    display: block;
+}
+
+.todo-controls {
+    display: block;
+    font-size: small;
+}
+
+.todo-container {
+    border-radius: 5px;
+    background: darkgray;
+    padding: 5px;
+    margin-top: 5px;
+    width: 600px;
+    text-align: center;
+}
+
+.btn {
+    border-radius: 5px;
+}
+</style>
